7111a3c8888bb02d0452936f61ee0148382fe94e
Luis Garcia <garcialuis1998@yahoo.es>
2020-03-25 16:49:23 -0400

    First commit.

---
+++ VR Prototyping Tool/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class MenuManager : MonoBehaviour
+{
+    public Canvas mainMenuCanvas;
+    public Canvas modifyMenuCanvas;
+    public Canvas addInteractionCanvas;
+    public Canvas chooseActionCanvas;
+    public Canvas deleteConfirmationCanvas;
+    public Canvas duplicationConfirmationCanvas;
+    public GameObject scalingSpheres;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideMenus();
+        HideScalingSpheres();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.GetComponent<Hand>() == null
+            || modifyMenuCanvas.enabled == true
+            || addInteractionCanvas.enabled == true)
+            return;
+
+        mainMenuCanvas.enabled = true;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        HideMenus();
+    }
+
+    public void HideMenus()
+    {
+        mainMenuCanvas.enabled = false;
+        modifyMenuCanvas.enabled = false;
+        addInteractionCanvas.enabled = false;
+        chooseActionCanvas.enabled = false;
+        deleteConfirmationCanvas.enabled = false;
+        duplicationConfirmationCanvas.enabled = false;
+        scalingSpheres.SetActive(false);
+    }
+
+    public void ShowModifyCanvas()
+    {
+        HideMenus();
+        modifyMenuCanvas.enabled = true;
+    }
+
+    public void ShowAddInteractionCanvas()
+    {
+        HideMenus();
+        addInteractionCanvas.enabled = false;
+    }
+
+    public void ShowChooseActionCanvas()
+    {
+        HideMenus();
+        chooseActionCanvas.enabled = true;
+    }
+
+    public void ShowDeleteConfirmationCanvas()
+    {
+        HideMenus();
+        deleteConfirmationCanvas.enabled = true;
+    }
+
+    public void ShowDuplicationConfirmationCanvas()
+    {
+        HideMenus();
+        duplicationConfirmationCanvas.enabled = true;
+    }
+
+    public void DestroyGameObjectObject()
+    {
+        Destroy(gameObject);
+    }
+
+    public void DuplicateGameObject()
+    {
+        Vector3 newPosition = transform.position + new Vector3(0f, 0.1f, 0f);
+        Instantiate(this, newPosition, transform.rotation, null);
+    }
+
+    public void HideScalingSpheres()
+    {
+        scalingSpheres.SetActive(false);
+    }
+
+    public void ShowScalingSpheres()
+    {
+        scalingSpheres.SetActive(true);
+    }
+}
---
+++ VR Prototyping Tool/Assets/Scripts/ScalingSphere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalingSphere : MonoBehaviour
+{
+    ScalingSphereManager scalingSphereManager;
+    Vector3 originalLocalPosition;
+
+    public void Start()
+    {
+        originalLocalPosition = transform.localPosition;
+    }
+
+    public void OnGrab()
+    {
+        scalingSphereManager.AddScalingSphere(this);
+    }
+
+    public void OnRelease()
+    {
+        scalingSphereManager.RemoveScalingSphere(this);
+        transform.localPosition = originalLocalPosition;
+    }
+}
